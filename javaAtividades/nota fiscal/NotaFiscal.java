1408a00771c9a9b69a80d5e8be3b621dab38caa0
Bea <beatrizheimann@gmail.com>
2025-07-14 10:59:21 -0300

    Update NotaFiscal.java

--- javaAtividades/nota fiscal/NotaFiscal.java
+++ javaAtividades/nota fiscal/NotaFiscal.java
@@ -1,11 +1,9 @@
 public class NotaFiscal {
-    // Atributos
     private String numeroPeca;
     private String descricaoPeca;
     private int quantidadeComprada;
     private double preco;
 
-    // Construtor
     public NotaFiscal(String numeroPeca, String descricaoPeca, int quantidadeComprada, double preco) {
         this.numeroPeca = numeroPeca;
         this.descricaoPeca = descricaoPeca;
@@ -13,12 +11,10 @@ public class NotaFiscal {
         this.preco = preco;
     }
 
-    // Método que retorna o total dessa nota (quantidade × preço)
     public double getTotal() {
         return quantidadeComprada * preco;
     }
 
-    // Método estático que recebe um vetor de notas e soma todos os totais
     public static double getTotalNota(NotaFiscal[] notas) {
         double totalGeral = 0;
 
